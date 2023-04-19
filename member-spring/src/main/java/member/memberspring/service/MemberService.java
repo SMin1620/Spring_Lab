e3bc023ab161a857d9f080bed7920d3f4bcec009
SMin1620 <ibb09@naver.com>
2023-04-20 02:03:40 +0900

    feat : Member Method Mapping and MemberList

--- member-spring/src/main/java/member/memberspring/service/MemberService.java
+++ member-spring/src/main/java/member/memberspring/service/MemberService.java
@@ -3,13 +3,17 @@ package member.memberspring.service;
 import member.memberspring.domain.Member;
 import member.memberspring.repository.MemberRepository;
 import member.memberspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
