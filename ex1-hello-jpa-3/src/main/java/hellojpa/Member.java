76b2b7fcb840290b43af3a6e5774c98ff39714f4
이승민 <81574795+SMin1620@users.noreply.github.com>
2023-06-14 18:27:40 +0900

    feat : jpa 단방향 연관관계 매핑, 1 : N, FK

---
+++ ex1-hello-jpa-3/src/main/java/hellojpa/Member.java
@@ -0,0 +1,54 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    @Column(name = "USERNAME")
+    private String name;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+
+    public Member() {}
+
+    public Member(Long id, String name, Team team) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
---
+++ ex1-hello-jpa-3/src/main/java/hellojpa/Team.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Team() {}
+    public Team(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
