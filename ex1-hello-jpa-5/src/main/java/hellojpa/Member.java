95d8466d1c0663e33ca1e9f61a86bbadaa81f8d1
이승민 <81574795+SMin1620@users.noreply.github.com>
2023-06-22 04:05:30 +0900

    feat : JPA 상속 관계 매핑

---
+++ ex1-hello-jpa-5/src/main/java/hellojpa/Member.java
@@ -0,0 +1,56 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member extends BaseEntity{
+
+    @Id @GeneratedValue
+    private Long id;
+    @Column(name = "USERNAME")
+    private String name;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+
+    public Member() {}
+
+    public Member(Long id, String name, Team team) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+
+        team.getMembers().add(this);
+    }
+}
